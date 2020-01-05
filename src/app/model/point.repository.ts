590b698a19269caba6882c2d3dadbd856b0ef2ee
Alisa <a.popova9@g.nsu.ru>
2020-01-06 00:41:43 +0700

    Add points data

---
+++ src/app/model/point.repository.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Point } from './point.model';
+import { StaticDataSource } from './static.datasource';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PointRepository {
+  private points: Point[] = [];
+
+  constructor(private dataSource: StaticDataSource) {
+    dataSource.getPoints().subscribe((data: Point[]) => {
+      this.points = data;
+    });
+  }
+
+  public getCards(): Point[] {
+    return this.points;
+  }
+}
