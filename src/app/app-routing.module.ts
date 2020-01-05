5377c2892e80360bcf3d12f6dbf96cd3e7d6e3de
Alisa <a.popova9@g.nsu.ru>
2020-01-05 18:38:44 +0700

    Add tabs routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PointsComponent } from "./points/points.component";
+import { CardsComponent } from "./cards/cards.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cards',
+    component: CardsComponent
+  },
+  {
+    path: 'points',
+    component: PointsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
